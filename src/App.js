c9f15b0c70537a9ae9a87f21bf6d865964d58012
scientechh <scientech.itcompany@gmail.com>
2023-05-01 18:11:23 +0400

    add

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import {Landing} from "./pages/landing/landing";
 import {Login} from "./pages/login/login";
 import {UserInfo} from "./pages/user/userInfo";
 import {UserDetalis} from "./pages/userDetalis/userDetalis";
+import {useState} from "react";
 
+const usersList = [
+    {id: Math.floor(Math.random() * 1000), name: "John", phone: "012133545", email: "john@mail.ru"},
+    {id: Math.floor(Math.random() * 1000), name: "Mike", phone: "016633545", email: "mike@mail.ru"},
+    {id: Math.floor(Math.random() * 1000), name: "Ellison", phone: "012144445", email: "elison@mail.ru"},
+    {id: Math.floor(Math.random() * 1000), name: "Jay", phone: "034454545", email: "jay@mail.ru"}
+]
 
 function App() {
-    const usersList = [
-        {id: Math.floor(Math.random() * 1000), name: "John", phone: "012133545", email: "john@mail.ru"},
-        {id: Math.floor(Math.random() * 1000), name: "Mike", phone: "016633545", email: "mike@mail.ru"},
-        {id: Math.floor(Math.random() * 1000), name: "Ellison", phone: "012144445", email: "elison@mail.ru"},
-        {id: Math.floor(Math.random() * 1000), name: "Jay", phone: "034454545", email: "jay@mail.ru"}
-    ]
+
+    const [userList, setUserList] = useState(usersList)
 
   return (
     <div>
       <Routes>
         <Route path={"/"} element={<Landing/>}/>
         <Route path={"/login"} element={<Login/>}/>
-        <Route path={"/user"} element={<UserInfo usersList={usersList}/>}/>
-        <Route path={"/userDetalis/:id"} element={<UserDetalis usersList={usersList}/>}/>
+        <Route path={"/user"} element={<UserInfo usersList={userList} setUserList={setUserList}/>}/>
+        <Route path={"/userDetalis/:id"} element={<UserDetalis usersList={userList}/>}/>
         <Route path={"*"} element={<Landing/>}/>
       </Routes>
     </div>
--- src/pages/user/userInfo.js
+++ src/pages/user/userInfo.js
@@ -1,19 +1,44 @@
 import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
 import {UserDetalis} from "../userDetalis/userDetalis";
+import {useState} from "react";
 
 
-export const UserInfo = ({usersList}) => {
+export const UserInfo = ({usersList, setUserList}) => {
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [phone, setPhone] = useState()
+
+    const AddUser = () => {
+        setUserList([...usersList,
+            {id: Math.floor(Math.random() * 1000), name: name, phone: phone, email: email}]
+            )
+    }
+
+    const RemoveUser = (e) => {
+        setUserList(usersList.filter((el) => {return el.id !== +e.target.id}))
+    }
 
     return(
         <div className={"users"}>
             <h2>Users</h2>
+            <div className={"users__add"}>
+                <input type="text" placeholder={"Name"} onChange={(e) => setName(e.target.value)}/>
+                <input type="email" placeholder={"Email"} onChange={(e) => setEmail(e.target.value)}/>
+                <input type="number" placeholder={"Phone"} onChange={(e) => setPhone(e.target.value)}/>
+                <button onClick={AddUser}>Add</button>
+            </div>
             <div className={"users__list"}>
             {
                 usersList?.map((el) => {
-                    return(<Link to={`/userDetalis/${el.id}`} key={el.id}>
-                        {el.name}
-                    </Link>)
+                    return(
+                        <div key={el.id} className={"users__list__user"}>
+                            <Link to={`/userDetalis/${el.id}`}>
+                                {el.name}
+                            </Link>
+                            <button id={el.id} onClick={RemoveUser}>Remove</button>
+                        </div>
+                    )
                 })
             }
             </div>
@@ -22,5 +47,6 @@ export const UserInfo = ({usersList}) => {
 }
 
 UserInfo.propTypes = {
-    usersList: PropTypes.array
+    usersList: PropTypes.array,
+    setUserList: PropTypes.func
 }
